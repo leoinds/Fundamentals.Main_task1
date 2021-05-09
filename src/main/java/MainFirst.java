975974eb2d007f5cf033a73e65f266a66e501253
Leonid Ermakovich <lermakovich@inbox.ru>
2021-05-10 02:04:49 +0300

    первый проект

---
+++ src/main/java/MainFirst.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MainFirst {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String username = scanner.next();
+        System.out.println ("Hello " + username);
+    }
+}
